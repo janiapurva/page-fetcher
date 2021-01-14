9068d63345f8d0b1ac5809ba5e2613e255aa81d0
janiapurva <68935328+janiapurva@users.noreply.github.com>
2021-01-14 09:38:11 -0500

    Update fetcher.js

--- fetcher.js
+++ fetcher.js
@@ -12,14 +12,16 @@ const request = require('request');
 request(`${url}`, (error, response, body) => {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the edu homepage.
+  let downData = body;// Print the HTML for the edu homepage.
   console.log(`Downloaded and saved 1235 bytes to ./index.html`);
+  fs.writeFile(`${localPath}`,downData, "utf8", (error) => {
+    //console.log(downData);
+    if (error) throw error;
+
+    console.log(`Downloaded and saved 1235 bytes to ./index.html`);
+  });
   
   // console.log(`Downloaded and saved 1235 bytes to ./index.html`);
 });
 
 
-fs.writeFile(`${localPath}`,'data', "utf8", (data) => {
-  console.log(data);
-  // console.log(`Downloaded and saved 1235 bytes to ./index.html`);
-});
